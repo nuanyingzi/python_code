78af90fe50af03288837e31643082abc49773c3d
tao <852947475@qq.com>
2024-06-12 17:52:13 +0800

    while循环

---
+++ venv/06_循环/while.py
@@ -0,0 +1,16 @@
+# while循环
+# 计算从1到100的整数和
+total = 0
+i = 1
+while  i <= 100:
+    total += i
+    i += 1
+print(total)
+
+# 计算从1到100的偶数求和
+total2 = 0
+j = 2
+while j <= 100:
+    total2 += j
+    j += 2
+print(total2)
