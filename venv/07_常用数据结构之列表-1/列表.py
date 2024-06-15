c8defdeb37abeb931874f9bdc600097485bb16bc
tao <852947475@qq.com>
2024-06-15 10:15:42 +0800

    列表

---
+++ venv/07_常用数据结构之列表-1/列表.py
@@ -0,0 +1,22 @@
+items5 = [35, 12, 99, 45, 66]
+items6 = [45, 58, 29]
+items7 = ['Python', 'Java', 'JavaScript']
+print(items5 + items6)  # [35, 12, 99, 45, 66, 45, 58, 29]
+print(items6 + items7)  # [45, 58, 29, 'Python', 'Java', 'JavaScript']
+items5 += items6
+print(items5)  # [35, 12, 99, 45, 66, 45, 58, 29]
+
+print(items6 * 3)  # [45, 58, 29, 45, 58, 29, 45, 58, 29]
+print(items7 * 2)  # ['Python', 'Java', 'JavaScript', 'Python', 'Java', 'JavaScript']
+
+items8 = ['apple', 'waxberry', 'pitaya', 'peach', 'watermelon']
+print(items8[0])   # apple
+print(items8[2])   # pitaya
+print(items8[4])   # watermelon
+items8[2] = 'durian'
+print(items8)      # ['apple', 'waxberry', 'durian', 'peach', 'watermelon']
+print(items8[-5])  # 'apple'
+print(items8[-4])  # 'waxberry'
+print(items8[-1])  # watermelon
+items8[-4] = 'strawberry'
+print(items8)      # ['apple', 'strawberry', 'durian', 'peach', 'watermelon']
